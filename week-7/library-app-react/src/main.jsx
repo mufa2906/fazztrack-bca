a3d806ffab5f06de01eb3c17d056dde1c5005008
Muhamad Farhan Junaldi <m.farhan2906@gmail.com>
2023-09-29 16:26:06 +0700

    auth page with token, redirect to /home

--- week-7/library-app-react/src/main.jsx
+++ week-7/library-app-react/src/main.jsx
@@ -2,26 +2,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import LoginForm from "./components/loginForm/LoginForm.jsx";
 import RegisForm from "./components/regisForm/RegisForm.jsx";
 import "./index.css";
 import Auth from "./pages/auth/index.jsx";
 import Detail from "./pages/detail/index.jsx";
 import Home from "./pages/home/index.jsx";
-
-const PrivateRoute = ({ element }) => {
-  const token = localStorage.getItem("token"); // Ambil token dari local storage atau dari mana pun Anda menyimpannya
-
-  // Periksa apakah pengguna memiliki token
-  if (!token) {
-    // Jika tidak ada token, kembalikan pengguna ke halaman utama atau halaman login
-    return <Navigate to="/" />;
-  }
-
-  // Jika pengguna memiliki token, izinkan akses ke elemen yang diberikan
-  return element;
-};
 
 const router = createBrowserRouter([
   {
@@ -40,11 +31,11 @@ const router = createBrowserRouter([
   },
   {
     path: "/home",
-    element: <PrivateRoute element={<Home />} />,
+    element: localStorage.getItem("token") ? <Home /> : <Navigate to="/" />,
   },
   {
     path: "/books/:bookId",
-    element: <PrivateRoute element={<Detail />} />,
+    element: localStorage.getItem("token") ? <Detail /> : <Navigate to="/" />,
   },
 ]);
 
--- week-7/library-app-react/src/pages/auth/index.jsx
+++ week-7/library-app-react/src/pages/auth/index.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
 import AuthLayout from "../../layout/auth/AuthLayout";
+import { Navigate } from "react-router-dom";
 
 function Auth() {
-  return (
+  const token = localStorage.getItem("token");
+  return token ? (
+    <Navigate to="/home" />
+  ) : (
     <AuthLayout>
       <Outlet />
     </AuthLayout>
